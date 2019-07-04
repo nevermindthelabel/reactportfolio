fd29062c25daf2e335094c1d495994f71930764a
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-03 22:48:58 -0700

    added initial client side react code

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return <div>Welcome to my Portfolio</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
