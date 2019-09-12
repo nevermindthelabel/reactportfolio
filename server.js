7072344814520b910d66b818a35289719e983bec
Matt Kilcup <nevermindthelabel@gmail.com>
2019-09-11 22:25:59 -0700

    changes to server configuration

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-const express = require("express");
-// const routes = require('./routes');
-const PORT = process.env.PORT || 5000
-
+require('dotenv').config();
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+
+app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
-app.get('/', (req, res) => {
-  res.send('Wecome to my API')
-})
-
 app.listen(PORT, () => {
-  console.log(
-    `🌎  ==> API Server now listening on PORT http://localhost:${PORT}`
-  );
+  console.log(`🌎  ==> API Server now listening on PORT http://localhost:${PORT}`);
 });
