67b1236385b3bca09b6aefffe1679ed4642490d7
Matt Kilcup <nevermindthelabel@gmail.com>
2019-09-16 22:04:36 -0700

    react router setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import './Pages/style.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <BrowserRouter>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/portfolio' component={Portfolio} />
+      <Route exact path='/contact' component={Contact} />
+      </BrowserRouter>
+    </Fragment>
+  );
+};
+
+export default App;
