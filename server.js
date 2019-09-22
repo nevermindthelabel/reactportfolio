e91dfd6d6d1d9aa5390f992e1c629e5d29a3ea9b
Matt Kilcup <nevermindthelabel@gmail.com>
2019-09-21 21:34:00 -0700

    added catchall route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT || 5000;
-// const routes = require('./routes');
+const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 
@@ -11,9 +12,13 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-// app.use(routes);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎  ==> API Server now listening on PORT http://localhost:${PORT}`);
