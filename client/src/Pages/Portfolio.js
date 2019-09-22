1e1be8424aa1a811048b2fefce4f9a00402a85a5
Matt Kilcup <nevermindthelabel@gmail.com>
2019-09-21 21:02:37 -0700

    updates to layout

--- client/src/Pages/Portfolio.js
+++ client/src/Pages/Portfolio.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+
 import Wrapper from '../components/Wrapper';
 import Navigation from '../components/Navigation';
 import ProfileCard from '../components/Cards';
@@ -9,19 +11,21 @@ const Portfolio = () => {
   return (
     <Fragment>
       <Navigation />
-      <div className="container">
-        <h1 className="mb-5 mt-5">Portfolio</h1>
-        <Wrapper>
-          {Projects.map(project => (
-            <ProfileCard
-              key={project.id}
-              image={project.image}
-              title={project.projectTitle}
-              text={project.projectText}
-            />
-          ))}
-        </Wrapper>
-      </div>
+      <Container className="portfolio" fluid="true">
+        <h1 className="p-5 text-white">Portfolio</h1>
+        <Container>
+          <Wrapper>
+            {Projects.map(project => (
+              <ProfileCard
+                key={project.id}
+                image={project.image}
+                title={project.projectTitle}
+                text={project.projectText}
+              />
+            ))}
+          </Wrapper>
+        </Container>
+      </Container>
       <Footer />
     </Fragment>
   );
