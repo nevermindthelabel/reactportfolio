f5df390cb3e583bb9abe43a54a408383edca7a09
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-30 20:22:34 -0700

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,8 @@ const App = () => {
   return (
     <Fragment>
       <BrowserRouter>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/portfolio' component={Portfolio} />
-      <Route exact path='/contact' component={Contact} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/portfolio' component={Portfolio} />        <Route exact path='/contact' component={Contact} />
       </BrowserRouter>
     </Fragment>
   );
